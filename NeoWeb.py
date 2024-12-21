63f49d73bccd6c5b1270f08b8bec3ef834387663
Program <67320703+Program132@users.noreply.github.com>
2024-12-21 16:46:07 +0100

    Tests

--- NeoWeb.py
+++ NeoWeb.py
@@ -1,6 +1,7 @@
 import signal
 import sys
 from Crawler import Crawler
+import Indexer
 
 def handle_interrupt(signal, frame):
     print("\nInterruption reçue (Ctrl+C). Sauvegarde en cours...")
@@ -10,5 +11,7 @@ def handle_interrupt(signal, frame):
 signal.signal(signal.SIGINT, handle_interrupt)
 
 print("Le programme est en cours d'exécution... Appuyez sur Ctrl+C pour interrompre.")
-crawler = Crawler("https://en.wikipedia.org/wiki/List_of_file_signatures")
+crawler = Crawler("https://en.wikipedia.org/wiki/List_of_file_signatures", max_iteration=10)
 crawler.crawl()
+
+Indexer.start_indexer()
