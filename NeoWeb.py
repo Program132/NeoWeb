0f8c40f82e6fc9fd1aa95d4bad30dacc47b0e201
Program <67320703+Program132@users.noreply.github.com>
2024-12-22 01:02:38 +0100

    Ignore

--- NeoWeb.py
+++ NeoWeb.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from JsonDatabase import JsonDatabase
 from Pertinence import get_final_value_page
-from bs4 import BeautifulSoup  # Pour extraire les titres de page HTML
 
 app = Flask(__name__)
 
@@ -15,15 +14,13 @@ def home():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form.get('query')
-    if not query:
-        return jsonify({"error": "Veuillez fournir une requête de recherche."}), 400
+    if not query: return jsonify({"error": "Veuillez fournir une requête de recherche."}), 400
 
     try:
         db_data = JsonDatabase(dataDB)
         all_urls = db_data.get_all_keys()
 
-        if not all_urls:
-            return jsonify({"error": "Aucune URL indexée dans la base de données."}), 404
+        if not all_urls: return jsonify({"error": "Aucune URL indexée dans la base de données."}), 404
 
         results = []
         for url in all_urls:
@@ -35,27 +32,21 @@ def search():
                 excerpt = extract_excerpt(record.get("text", ""))
 
                 results.append({"url": url, "title": title, "excerpt": excerpt, "score": score})
-            except ValueError as e:
-                print(f"Erreur pour l'URL {url}: {e}")
+            except ValueError as e: print(f"Erreur pour l'URL {url}: {e}")
 
         results = sorted(results, key=lambda x: x['score'], reverse=True)
 
         return render_template('index.html', results=results)
-    except FileNotFoundError:
-        return jsonify({"error": "Le fichier data.json est introuvable."}), 500
-    except Exception as e:
-        print(f"Erreur inattendue : {e}")
-        return jsonify({"error": f"Erreur lors du traitement de la requête : {str(e)}"}), 500
+    except FileNotFoundError: return jsonify({"error": "Le fichier data.json est introuvable."}), 500
+    except Exception as e: return jsonify({"error": f"Erreur lors du traitement de la requête : {str(e)}"}), 500
 
 
 def extract_title(record):
     return record["title"]
 
-
 def extract_excerpt(text_page):
     lines = text_page.splitlines()
-    return ' '.join(lines[:3])  # Joindre les trois premières lignes
-
+    return ' '.join(lines[:3])
 
 if __name__ == '__main__':
     app.run(debug=True)
