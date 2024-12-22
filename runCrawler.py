016be1df33963e80c84a8e75cb90e72c66780c35
Program <67320703+Program132@users.noreply.github.com>
2024-12-22 01:03:57 +0100

    Set URL as arg in runCrawler.py

--- runCrawler.py
+++ runCrawler.py
@@ -1,13 +1,18 @@
 from Crawler import Crawler
 import signal
-import sys
+import argparse
 
 def handle_stop_signal(signum, frame):
     print("\nInterruption reçue. L'indexation va être arrêtée.")
     crawler.handle_stop_signal()
 
+
 if __name__ == '__main__':
-    url = "https://fr.wikipedia.org/"
+    parser = argparse.ArgumentParser(description="Crawler de site Web")
+    parser.add_argument('--url', type=str, required=True, help="L'URL de départ pour le crawling")
+
+    args = parser.parse_args()
+    url = args.url
 
     crawler = Crawler(start_url=url)
     signal.signal(signal.SIGINT, handle_stop_signal)
